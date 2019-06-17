e328836ecf3c59994e55bc8ccaf95372fbb05308
Austin West <korruptedanomaly@gmail.com>
2019-06-17 12:23:39 -0600

    Complete App

--- SettingsiOS27/SettingsiOS27/CONTROLLERS/VIEW CONTROLLERS/CUSTOM VIEWS/SettingTableViewCell.swift
+++ SettingsiOS27/SettingsiOS27/CONTROLLERS/VIEW CONTROLLERS/CUSTOM VIEWS/SettingTableViewCell.swift
@@ -8,26 +8,40 @@
 
 import UIKit
 
+// This is the protocol
+protocol SettingCellDelegate: class {
+    func settingSwitchTapped(for cell: SettingTableViewCell)
+}
+
+
 class SettingTableViewCell: UITableViewCell {
     
     @IBOutlet weak var settingIconImageView: UIImageView!
     @IBOutlet weak var settingNameLabel: UILabel!
     @IBOutlet weak var settingSwitch: UISwitch!
     
+    //    Define the delegate
+    weak var cellDelegate: SettingCellDelegate?
     
     func updateViews(with setting: Setting) {
         settingIconImageView.image = setting.icon
         settingNameLabel.text = setting.name
         settingSwitch.isOn = setting.isOn
         
-    /*    if settingSwitch.isOn == true {
-            self.backgroundColor = UIColor.orange
-        } else {
-            self.backgroundColor = UIColor.white
-        }
- */
+        //        This also works
+        /*    if settingSwitch.isOn == true {
+         self.backgroundColor = UIColor.orange
+         } else {
+         self.backgroundColor = UIColor.white
+         }
+         */
         
-//        turnary operator
+        //        turnary operator
         self.backgroundColor = settingSwitch.isOn ? .orange : .white
     }
+    
+    @IBAction func settingCellToggled(_ sender: Any) {
+        cellDelegate?.settingSwitchTapped(for: self)
+    }
+    
 }
--- SettingsiOS27/SettingsiOS27/CONTROLLERS/VIEW CONTROLLERS/SettingListTableViewController.swift
+++ SettingsiOS27/SettingsiOS27/CONTROLLERS/VIEW CONTROLLERS/SettingListTableViewController.swift
@@ -9,27 +9,41 @@
 import UIKit
 
 class SettingListTableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return SettingController.sharedInstance.settings.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell", for: indexPath) as? SettingTableViewCell else { return UITableViewCell() }
-
-        let setting = SettingController.sharedInstance.settings[indexPath.row]
-        cell.updateViews(with: setting)
         
+        let setting = SettingController.sharedInstance.settings[indexPath.row]
+        //        Actually setting the delegate, Do not forget this!
+        cell.cellDelegate = self
+        cell.updateViews(with: setting)
         return cell
     }
-
+    
+} // End of class
+//conforming to protocol
+extension SettingListTableViewController: SettingCellDelegate {
+    func settingSwitchTapped(for cell: SettingTableViewCell) {
+        //        Find the index path of the cell
+        guard let indexPath = tableView.indexPath(for: cell) else { return }
+        //        Using that index path - find the setting
+        let setting = SettingController.sharedInstance.settings[indexPath.row]
+        //        toggle the isOn for the setting
+        SettingController.sharedInstance.toggleIsOn(for: setting)
+        //        update the cells view
+        cell.updateViews(with: setting)
+    }
 }

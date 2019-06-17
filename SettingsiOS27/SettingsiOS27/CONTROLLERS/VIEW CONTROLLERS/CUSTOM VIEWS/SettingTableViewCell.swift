214c136d7eefac6c4d70328a2220e011f0a3f8bb
Austin West <korruptedanomaly@gmail.com>
2019-06-17 11:58:21 -0600

    App functions all except the switchTapped

--- SettingsiOS27/SettingsiOS27/CONTROLLERS/VIEW CONTROLLERS/CUSTOM VIEWS/SettingTableViewCell.swift
+++ SettingsiOS27/SettingsiOS27/CONTROLLERS/VIEW CONTROLLERS/CUSTOM VIEWS/SettingTableViewCell.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class SettingTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    @IBOutlet weak var settingIconImageView: UIImageView!
+    @IBOutlet weak var settingNameLabel: UILabel!
+    @IBOutlet weak var settingSwitch: UISwitch!
+    
+    
+    func updateViews(with setting: Setting) {
+        settingIconImageView.image = setting.icon
+        settingNameLabel.text = setting.name
+        settingSwitch.isOn = setting.isOn
+        
+    /*    if settingSwitch.isOn == true {
+            self.backgroundColor = UIColor.orange
+        } else {
+            self.backgroundColor = UIColor.white
+        }
+ */
+        
+//        turnary operator
+        self.backgroundColor = settingSwitch.isOn ? .orange : .white
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }

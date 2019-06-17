a523364cadb3719187c456697d1be26aae35fd47
Austin West <korruptedanomaly@gmail.com>
2019-06-17 11:22:48 -0600

    SettingController finished

--- SettingsiOS27/SettingsiOS27/CONTROLLERS/MODEL CONTROLLERS/SettingController.swift
+++ SettingsiOS27/SettingsiOS27/CONTROLLERS/MODEL CONTROLLERS/SettingController.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+class SettingController {
+    
+//    Singleton
+    static let sharedInstance = SettingController()
+    
+    let settings: [Setting] = {
+        let music = Setting(name: "iTunes", icon: #imageLiteral(resourceName: "iTunes"), isOn: false)
+        let appStore = Setting(name: "App Store", icon: #imageLiteral(resourceName: "appStore"), isOn: false)
+        let iBooks = Setting(name: "iBooks", icon: #imageLiteral(resourceName: "iBooks"), isOn: true)
+        
+        return [music, appStore, iBooks]
+    } ()
+    
+    func toggleIsOn(for setting: Setting) {
+        setting.isOn = !setting.isOn
+    }
+    
+}
